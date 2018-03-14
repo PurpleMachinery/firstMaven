57ae6c78edb8c871a9935229c2f005ef0ac562e5
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2018-03-14 20:06:56 -0300

    mvn inicializado

---
+++ helloworld/src/main/java/br/com/eteczonaleste/App.java
@@ -0,0 +1,16 @@
+package br.com.eteczonaleste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String[] A , B ;
+    	A[0]="Teste";
+    	B=A.clone();
+        System.out.println( "Hello World!"+B[0] );
+    }
+}
